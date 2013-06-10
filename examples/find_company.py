686c4766c7ae5d3705185432db38d2bd43f22adb
vtemian <vladtemian@gmail.com>
2013-06-10 19:12:46 +0300

    Added example for company retrieve

---
+++ examples/find_company.py
@@ -0,0 +1,8 @@
+from pynkedin import Company
+from pynkedin.lib.auth import AuthSession
+
+token = 'AQWL69--MU24jYiZZ1RpHDYF4CTrYoLNM30bstwKtD8GvYPqyfDDiA-QBbMO3Nx5-NnFxh-EKIiIbAxqOm9IYnUerIPlhzEJHmXn6RuzOqxqTBoAWDBEUq7YEFnw-e0X5lIq4mmuxoxm_JAIXrjOZ0ccOqp10vyqQ1kaOv4e0tKYnY0GLxQ'
+
+AuthSession(client_id='hbm5ioyd4zvu', client_secret='1kDH3xYwW2D5Qwa3', access_token=token)
+
+company = Company.find(id=7)
