d0735deb3a38645dc1257c0d4c04fee45d80aaac
vtemian <vladtemian@gmail.com>
2013-06-10 19:07:03 +0300

    Refactor access_token retrieve and sessions creation

--- examples/token_retrieve.py
+++ examples/token_retrieve.py
@@ -1,21 +1,26 @@
 from colorama import Fore
 
-from pynkedin.lib.auth import Auth
+from pynkedin.lib.auth import AuthService
 
 client_id = 'hbm5ioyd4zvu'
 client_secret = '1kDH3xYwW2D5Qwa3'
 
-linkedin = Auth(client_id, client_secret)
-
 print Fore.YELLOW + '---- START TOKEN RETRIEVING OPERATION ----' + Fore.RESET
+#scope         = raw_input(Fore.RED + 'Define a scope: ' + Fore.RESET)
+#redirect_uri  = raw_input(Fore.RED + 'Use a redirect url: ' + Fore.RESET)
+scope = 'r_fullprofile'
+redirect_uri = 'http://linkedin.gosimplysocial.com'
 
-scope         = raw_input(Fore.RED + 'Define a scope: ' + Fore.RESET)
-redirect_uri  = raw_input(Fore.RED + 'Use a redirect url: ' + Fore.RESET)
 
-url = linkedin.get_token_url(scope, redirect_uri)
+service = AuthService(client_id, client_secret, redirect_uri)
+
+url = service.get_authorize_url(scope)
 print Fore.GREEN + 'Access this url and retrieve the token: ' + Fore.RESET + url 
 
-token = raw_input(Fore.GREEN + 'Paste the token from redirected url: ' + Fore.RESET)
-linkedin.set_token(token)
+auth_code = raw_input(Fore.GREEN + 'Paste the code from the redirected url: ' + Fore.RESET)
+access_token = service.get_access_token(auth_code)
+print Fore.GREEN + 'Acess TOKEN: ' + Fore.RESET + access_token 
 
-#Do stuff with your tokens
+api = service.create_session(access_token)
+
+#Do stuff with your session
