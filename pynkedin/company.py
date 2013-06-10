88e385e3752b1dd7e6f630df4c5c24da4238502e
vtemian <vladtemian@gmail.com>
2013-06-10 11:49:14 +0300

    Rauth wrapper for linkedin

--- pynkedin/company.py
+++ pynkedin/company.py
@@ -1,19 +1,19 @@
 class Company(object):
   """
     company = Company.find(id=1)
-    
+
     Retrieve
     --------
       company.updates => [{ update_1 }, { update_2 }]
       company.name    => 'Company name'
       company.fields  => { 'id': 1, ... , 'field_name': value }
-  
+
     Create
     ------
       company.updates.add(update)
       company.name = 'My awesome company'
 
   """
-  
+
   def find(self, *args):
     pass
