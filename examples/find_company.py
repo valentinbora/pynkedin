f1b4756311628c54dab47c28cad90c0d6ff9e208
vtemian <vladtemian@gmail.com>
2013-06-11 09:57:52 +0300

    Complete find comapany example with more args

--- examples/find_company.py
+++ examples/find_company.py
@@ -5,4 +5,4 @@ token = 'AQWL69--MU24jYiZZ1RpHDYF4CTrYoLNM30bstwKtD8GvYPqyfDDiA-QBbMO3Nx5-NnFxh-
 
 AuthSession(client_id='hbm5ioyd4zvu', client_secret='1kDH3xYwW2D5Qwa3', access_token=token)
 
-company = Company.find(id=7)
+company = Company.find(id=7, universal_name='apple')
