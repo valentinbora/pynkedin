2cc1399d019547ebf6d5ab7b341bab8153c150d0
vtemian <vladtemian@gmail.com>
2013-06-13 11:11:43 +0300

    Introduce response object to parser

--- pynkedin/parsers/company_parser.py
+++ pynkedin/parsers/company_parser.py
@@ -1,6 +1,14 @@
+import json
+from pprint import pprint as pp
+
 from parser import Parser
 
 class CompanyParser(Parser):
 
   def __call__(self, content):
-    print 'yee'
+    content = json.loads(content)
+
+    if not content["_total"]:
+      raise LookupError("Company not found!")
+
+    return self.to_object(content["values"][0])
--- pynkedin/parsers/parser.py
+++ pynkedin/parsers/parser.py
@@ -1,8 +1,16 @@
 from abc import ABCMeta, abstractmethod
 
+from response_object import ResponseObject
+
 class Parser(object):
   __metaclass__ = ABCMeta
+
+  def __init__(self, cls):
+    self.cls = cls
 
   @abstractmethod
   def __call__(self, content):
     pass
+
+  def to_object(self, response):
+    return ResponseObject(response)
