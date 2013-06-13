4e8447b1ffba5a55f25dd684b7f9893055badf14
vtemian <vladtemian@gmail.com>
2013-06-13 10:53:10 +0300

    Introduce response_object to company class

--- pynkedin/company.py
+++ pynkedin/company.py
@@ -7,7 +7,7 @@ FILTERS = ['email_domains']
 FIELDS  = ['id', 'name', 'universal-name', 'email-domains', 'company-type',
            'ticker', 'website-url', 'industries', 'status', 'logo-url',
            'square-logo-url', 'blog-rss-url', 'twitter-id',
-           'employee-count-range', 'specialties', 'locations', 'description', 
+           'employee-count-range', 'specialties', 'locations', 'description',
            'stock-exchange', 'founded-year', 'end-year', 'num-followers' ]
 
 class Company(object):
@@ -26,9 +26,14 @@ class Company(object):
       company.name = 'My awesome company'
 
   """
-  parser = CompanyParser()
 
   @classmethod
   def find(cls, **kwargs):
+    parser = CompanyParser(cls)
+
     if len(kwargs) > 1:
-      AuthSession().filter(path="companies", fields=FIELDS, parser=cls.parser, **kwargs)
+      response_object = AuthSession().filter(path="companies", fields=FIELDS, parser=parser, **kwargs)
+      return cls(response_object)
+
+  def __init__(self, response_object):
+    response_object.set_for(self)
