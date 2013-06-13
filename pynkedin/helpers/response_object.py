623db43e8f7a29bd26ea4d9b127966bc7c2422f2
vtemian <vladtemian@gmail.com>
2013-06-13 18:20:53 +0300

    Go back to dict based object

--- pynkedin/helpers/response_object.py
+++ pynkedin/helpers/response_object.py
@@ -1,7 +1,20 @@
-class ResponseObject(object):
+class ResponseObject(dict):
 
   def __init__(self, *args, **kwargs):
     super(ResponseObject, self).__init__(*args, **kwargs)
-  
+
+    self.__dict__ = self._check_for_inception(self)
+
+  def _check_for_inception(self, root_dict):
+
+    for key in root_dict:
+      if type(root_dict[key]) == dict:
+        if not 'values'in root_dict[key]:
+          root_dict[key] = ResponseObject(root_dict[key])
+        else:
+          root_dict[key] = root_dict[key]['values']
+
+    return root_dict
+
   def set_for(self, cls):
     cls.__dict__ = self.__dict__
