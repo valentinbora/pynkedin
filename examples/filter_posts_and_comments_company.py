7f09a18f2fb6d73d4f6cd2df3ee70afa0c70f100
vtemian <vladtemian@gmail.com>
2013-06-25 17:29:35 +0300

    Show how nested filtering is done

---
+++ examples/filter_posts_and_comments_company.py
@@ -0,0 +1,19 @@
+from pynkedin import Company
+from pynkedin.auth import AuthSession
+
+token = 'users_access_token'
+
+AuthSession(client_id='client_id_of_my_awesome_app',
+            client_secret='client_secret_of_my_awesome_app', 
+            access_token=token)
+
+company = Company(company_id='company_id', cache=True)
+
+
+# you can do nested filtering
+# first filter the posts and then the comments
+comments = company.updates.filter(snid='s5750272081627451398')[0].comments.filter(id=143903254)
+
+# iterate through filtered comments
+for comment in comments:
+  print comment
