02687ba01e5c0b677e1b85e04dddbfd04ac2a475
vtemian <vladtemian@gmail.com>
2013-06-26 15:53:25 +0300

    Test the parser with 404 status code as a response

--- pynkedin/tests/test_parser.py
+++ pynkedin/tests/test_parser.py
@@ -1,11 +1,15 @@
 import json
 
-from nose.tools import eq_
+from nose.tools import eq_, raises
 from mock import MagicMock
 
+from pynkedin.exceptions import NotFoundException
 from pynkedin.parser import Parser
 
 def test_parser_no_content():
+  '''
+    Test the parser without any response
+  '''
 
   mocked_response = MagicMock()
   mocked_response.content = json.dumps({'status_code': 123})
@@ -13,3 +17,16 @@ def test_parser_no_content():
 
   parser = Parser()
   eq_(parser(mocked_response), None)
+
+@raises(NotFoundException)
+def test_parser_error_raising():
+  '''
+    Test parser when the response contains 404
+  '''
+
+  mocked_response = MagicMock()
+  mocked_response.content = json.dumps({'content': 'nothing here'})
+  mocked_response.status_code = 404
+
+  parser = Parser()
+  parser(mocked_response)
